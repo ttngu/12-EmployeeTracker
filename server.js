f4dd5279e6315bdac2fb8009b1afa6ce6fdc4c0b
ttngu <tammyn137@gmail.com>
2020-12-07 20:26:10 -0800

    added server boiler plate code

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+var mysql = require("mysql");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// MySQL DB Connection Information 
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "harrypotter",
+  database: "employee_tracker_db"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Routes
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
