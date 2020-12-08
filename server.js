fed8dbec57e303e31939ada9f4b0cef651212cde
ttngu <tammyn137@gmail.com>
2020-12-07 20:58:41 -0800

    added express

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // Dependencies
+var express = require("express")
 var mysql = require("mysql");
+
+// Create express app instance.
+var app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
