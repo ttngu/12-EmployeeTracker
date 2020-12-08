d83f8f562c103537d1262df9375fb2615524b1c1
ttngu <tammyn137@gmail.com>
2020-12-07 20:55:32 -0800

    added all tables

--- seed.sql
+++ seed.sql
@@ -4,11 +4,37 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
+-- Department Table
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) NOT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO department (id, name)
-VALUES ("chicken_nugget", "Anna");
+INSERT INTO department (name)
+VALUES ("Anna");
+
+-- Role Table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(45) NOT NULL,
+  department_id INT(45) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 90000, "Sales");
+
+-- Employee Table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT(30) NOT NULL,
+  manager_id INT(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sarah", "Park", "Temp", "");
